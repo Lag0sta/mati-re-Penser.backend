159611d1c678e20e9ec77cc0b3e829108ce645ca
LRonnet <leo.ronnet@gmail.com>
2025-07-21 02:05:55 +0200

    serverless-http

--- api/index.ts
+++ api/index.ts
@@ -1,34 +1,21 @@
-import app from '../src/app'
-import { connectToDatabase } from '../src/models/connection'
 import { VercelRequest, VercelResponse } from '@vercel/node'
 import dotenv from 'dotenv'
+import app from '../src/app'
+import { connectToDatabase } from '../src/models/connection'
+import serverless from 'serverless-http'
 
 dotenv.config()
 
 let isConnected = false
 
-function runMiddleware(req: VercelRequest, res: VercelResponse) {
-  return new Promise<void>((resolve, reject) => {
-    app(req as any, res as any, (result: any) => {
-      if (result instanceof Error) {
-        return reject(result)
-      }
-      resolve()
-    })
-  })
-}
+const handler = serverless(app)
 
-export default async function handler(req: VercelRequest, res: VercelResponse) {
+export default async function (req: VercelRequest, res: VercelResponse) {
   if (!isConnected) {
     const connStr = process.env.CONNECTION_STRING || ''
     await connectToDatabase(connStr)
     isConnected = true
   }
 
-  try {
-    await runMiddleware(req, res)
-  } catch (error) {
-    console.error('Error in Express handler:', error)
-    res.status(500).json({ error: 'Internal Server Error' })
-  }
+  return handler(req, res)
 }
