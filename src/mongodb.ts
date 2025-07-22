198b21cfc7ff9d1ed427ec023b2741c088ac715c
LRonnet <leo.ronnet@gmail.com>
2025-07-22 02:54:48 +0200

    connecTimeoutMS:2000

--- src/mongodb.ts
+++ src/mongodb.ts
@@ -1,7 +1,5 @@
 import mongoose from 'mongoose';
 
-let isConnected = false;
-
 export async function connectToDatabase(uri: string) {
   if (mongoose.connection.readyState === 1) {
     console.log('✅ MongoDB déjà connecté (Mongoose)');
@@ -11,7 +9,7 @@ export async function connectToDatabase(uri: string) {
     throw new Error('La chaîne de connexion MongoDB est vide');
   }
   try {
-    await mongoose.connect(uri);
+    await mongoose.connect(uri, {connectTimeoutMS: 2000});
     console.log('✅ MongoDB connecté avec Mongoose');
   } catch (error) {
     console.error('❌ Erreur connexion MongoDB:', error);
