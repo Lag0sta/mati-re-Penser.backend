b66c4ec4b71b2b09cd4fe1b857544f618d32093a
LRonnet <leo.ronnet@gmail.com>
2025-09-02 13:47:23 +0200

    signUp rate-limite + MX verification

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,6 +1,10 @@
 import { Router } from 'express';
 import User from '../models/users';
 
+import rateLimit from "express-rate-limit";
+import dns from "dns/promises";
+
+
 import { checkToken } from '../utils/authActions';
 
 const router = Router();
@@ -17,8 +21,28 @@ router.get('/', (req, res) => {
   });
 });
 
+// 1Configuration du rate limiter
+const signupLimiter = rateLimit({
+  windowMs: 60 * 60 * 1000, // 1 heure
+  max: 5, // 5 inscriptions max par IP
+  message: { success: false, reason: "Trop de tentatives, réessayez plus tard." },
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+//Vérification MX
+async function hasValidMX(email: string): Promise<boolean> {
+  const domain = email.split("@")[1];
+  try {
+    const records = await dns.resolveMx(domain);
+    return records && records.length > 0;
+  } catch {
+    return false;
+  }
+}
+
 // Création d'un nouvel utilisateur
-router.post('/signup', async (req, res) => {
+router.post('/signup', signupLimiter, async (req, res) => {
   try {
     const { pseudo, email, password, confirmPassword, name, surname, hp } = req.body;
     console.log('➡️ [POST] /signup - Tentative de création de compte');
@@ -34,6 +58,20 @@ router.post('/signup', async (req, res) => {
       return;
     }
 
+    //vérification de syntaxe du mail
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    if (!emailRegex.test(email)) {
+      console.warn('⚠️ Email invalide');
+      res.json({ result: false, error: "adresse @mail invalide" });
+      return;
+    }
+    
+    //vérification du MX
+    if (!(await hasValidMX(email))) {
+      res.json({ success: false, reason: "Domaine email invalide" });
+      return
+    }
+
     const userData = await User.findOne({
       $or: [{ pseudo: pseudo }, { email: email }],
     });
@@ -43,12 +81,7 @@ router.post('/signup', async (req, res) => {
       return;
     }
 
-    const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/gi;
-    if (!emailRegex.test(email)) {
-      console.warn('⚠️ Email invalide');
-      res.json({ result: false, error: "adresse @mail invalide" });
-      return;
-    }
+    
 
     if (password !== confirmPassword) {
       console.warn('⚠️ Mots de passe non correspondants');
